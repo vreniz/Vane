d42cb8c9ca9234c45bac87834e6db155fb172bf4
vreniz <73375623+vreniz@users.noreply.github.com>
2026-05-06 17:40:52 -0500

    Create Vane.js

---
+++ Vane.js
@@ -0,0 +1,15 @@
+if (operacion == "+") {
+  let resultado = n1 + n2;
+  console.log(`El resultado de la operación es: ${resultado}`);
+} else if (operacion == "-") {
+  let resultado = n1 - n2;
+  console.log(`El resultado de la operación es: ${resultado}`);
+} else if (operacion == "*") {
+  let resultado = n1 * n2;
+  console.log(`El resultado de la operación es: ${resultado}`);
+} else if (operacion == "/") {
+  let resultado = n1 / n2;
+  console.log(`El resultado de la operación es: ${resultado}`);
+} else {
+  console.log("Opción Incorrecta");
+}
